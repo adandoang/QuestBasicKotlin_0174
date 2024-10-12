0f3f8f0ef68b09a67a5f249e3c73e216568dc2a0
adandoang <akhdanjzif@gmail.com>
2024-10-12 20:28:37 +0700

    data class finish

--- app/src/main/java/com/example/myapplication/DataClass.kt
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -5,3 +5,16 @@ data class DataClass(
     var email: String
 )
 
+fun main() {
+    val data = DataClass(1, "Adan@gmail.com")
+    println(data)
+
+    val data2 = DataClass(1, "Lara@gmail.com")
+    println(data == data2)
+
+    val data3 = data.copy()
+    println(data3)
+
+    val data4 = data.copy(email = "Yazidun@gmail.com")
+    println(data4)
+}
