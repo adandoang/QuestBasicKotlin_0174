9e995637ac9f6796fac9d5f16b2a6d18ab340378
adandoang <akhdanjzif@gmail.com>
2024-10-12 20:39:48 +0700

    main n finish

--- app/src/main/java/com/example/myapplication/Class.kt
+++ app/src/main/java/com/example/myapplication/Class.kt
@@ -4,3 +4,12 @@ class Motor()
 
 class Contact(val id: Int, var email: String)
 
+fun main() {
+    val contact = Contact(1, "Lara@gmail.com")
+
+    println(contact.email)
+
+    contact.email = "Adan@gmail.com"
+
+    println(contact.email)
+}
