39f7f137addf68cd5e7f4175504dcbf48839c92a
adandoang <akhdanjzif@gmail.com>
2024-10-12 20:22:36 +0700

    functions finish

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -10,3 +10,26 @@ fun withParameter(name: String) {
     println("== withParameter ==")
     println("Hello, $name!")
 }
+
+fun withNamedArgument(name: String, age: Int) {
+    println()
+    println("== withNamedArgument ==")
+    println("Hello, $name! You are $age years old.")
+}
+
+fun withDefaultParameter(name: String = "Adan", age: Int) {
+    println()
+    println("== wtihDefaultParameter ==")
+    println("Hello, $name You are $age Years old.")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParameter()
+    withParameter("Lara")
+    withNamedArgument(name = "Lara", age = 21)
+    withDefaultParameter(age = 21)
+}
