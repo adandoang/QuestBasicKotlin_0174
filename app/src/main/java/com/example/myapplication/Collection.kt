f3ac50e285bb3643d2a7d4048ea64864ccf167c5
adandoang <akhdanjzif@gmail.com>
2024-10-12 19:47:47 +0700

    Collection Set List Map

--- app/src/main/java/com/example/myapplication/Collection.kt
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -29,18 +29,44 @@ fun ContohList() {
 }
 
 fun ContohSet() {
+    println()
     println("=== Set ===")
 
-    //List Read-Only
+    //Set Read-Only
     val readOnlyAbjad = setOf("A", "B", "C")
     println(readOnlyAbjad)
 
-    //List Mutable
+    //Set Mutable
     val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
     println(shape)
 
-    //Menambahkan data ke list mutable
+    //Menambahkan data ke Set mutable
     shape.add("Rectangle")
+    println(shape)
+
+    //Menghapus data dari Set mutable
+    shape.remove("Circle")
+    println (shape)
+
+    //Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+
+fun ContohMap() {
+    println()
+    println("=== Map ===")
+
+    //List Read-Only
+    val readOnlyShape : Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    //List Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    //Menambahkan data ke list mutable
+    shape["Rectangle"] = 4
     println(shape)
 
     //Menghapus data dari list mutable
@@ -48,11 +74,12 @@ fun ContohSet() {
     println (shape)
 
     //list Read-Only
-    val shapesLocked: Set<String> = shape
+    val shapesLocked: Map<String, Int> = shape
     println(shapesLocked)
 }
 
 fun main() {
     ContohList()
     ContohSet()
+    ContohMap()
 }
